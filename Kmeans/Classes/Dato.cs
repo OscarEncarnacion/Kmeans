b814607e954f7eb0c2a12e33d7761fbcaee954c4
Oscar Encarnacion <oscar0alejandro@gmail.com>
2023-12-05 14:58:06 -0600

    Version Funcional 1.0

---
+++ Kmeans/Classes/Dato.cs
@@ -0,0 +1,20 @@
+﻿namespace Kmeans.Classes
+{
+    internal class Dato
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Dato()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Dato(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
